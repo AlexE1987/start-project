5dbb349b6d09fe4b670405d886914a4ec060a5bd
AlexE1987 <evsikovad1987@gmail.com>
2021-10-16 15:52:11 +0400

    refactor: replace class components whith functional(Form)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,65 +1,60 @@
-import React, { Component } from 'react';
 import FormCompleted from '../FormCompleted/FormCompleted';
 import Form from '../UI/Form/Form';
 import './Main.css';
 import { formInitialState, formErrors } from '../../store/store';
+import { useState } from 'react';
 
-export class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = formInitialState;
-  }
+import React from 'react';
 
-  getValuesFromForm = ({ target: { value, name } }) => {
-    this.setState(() => ({
-      [name]: value,
-    }));
-  };
+const Main = (props) => {
+  const [state, setState] = useState(formInitialState);
+  const getValuesFromForm = ({ target: { value, name } }) =>
+    setState((prevState) => ({ ...prevState, [name]: value }));
 
-  inputsValidate = () => {
-    if (!/^[A-ZА-Я]/.test(this.state.name.trim())) {
-      this.state.name === ''
+  const inputsValidate = () => {
+    if (!/^[A-ZА-Я]/.test(state.name.trim())) {
+      state.name === ''
         ? (formErrors.name = 'Поле пустое. Заполните пожалуйста')
         : (formErrors.name = 'Имя должно начинаться с заглавной буквы');
     } else formErrors.name = '';
 
-    if (!/^[A-ZА-Я]/.test(this.state.lastName.trim())) {
-      this.state.lastName === ''
+    if (!/^[A-ZА-Я]/.test(state.lastName.trim())) {
+      state.lastName === ''
         ? (formErrors.lastName = 'Поле пустое. Заполните пожалуйста')
         : (formErrors.lastName = 'Имя должно начинаться с заглавной буквы');
     } else formErrors.lastName = '';
 
-    if (this.state.dateOfBirth === '') {
+    if (state.dateOfBirth === '') {
       formErrors.dateOfBirth = 'Выберите дату рождения';
     } else formErrors.dateOfBirth = '';
 
-    if (!/\d\-\d{4}\-\d{2}\-\d{2}$/.test(this.state.phone.trim())) {
-      this.state.phone === ''
+    if (!/\d\-\d{4}\-\d{2}\-\d{2}$/.test(state.phone.trim())) {
+      state.phone === ''
         ? (formErrors.phone = 'Поле пустое. Заполните пожалуйста')
         : (formErrors.phone = 'Введите корректный номер телефона в формате: 7-7777-77-77');
     } else formErrors.phone = '';
 
-    if (this.state.site.trim().substring(0, 8) !== 'https://') {
-      this.state.site === ''
+    if (state.site.trim().substring(0, 8) !== 'https://') {
+      state.site === ''
         ? (formErrors.site = 'Поле пустое. Заполните пожалуйста')
         : (formErrors.site = 'Имя сайта должно начинаться с: "https://"');
     } else formErrors.site = '';
 
-    if (this.state.about.trim() === '') {
+    if (state.about.trim() === '') {
       formErrors.about = 'Поле пустое. Заполните пожалуйста';
-    } else if (this.state.about.trim().length > 600) {
+    } else if (state.about.trim().length > 600) {
       formErrors.about = 'Превышен лимит символов в поле ввода';
     } else formErrors.about = '';
 
-    if (this.state.stackTechnology.trim() === '') {
+    if (state.stackTechnology.trim() === '') {
       formErrors.stackTechnology = 'Поле пустое. Заполните пожалуйста';
-    } else if (this.state.stackTechnology.trim().length > 600) {
+    } else if (state.stackTechnology.trim().length > 600) {
       formErrors.stackTechnology = 'Превышен лимит символов в поле ввода';
     } else formErrors.stackTechnology = '';
 
-    if (this.state.lastProject.trim() === '') {
+    if (state.lastProject.trim() === '') {
       formErrors.lastProject = 'Поле пустое. Заполните пожалуйста';
-    } else if (this.state.lastProject.trim().length > 600) {
+    } else if (state.lastProject.trim().length > 600) {
       formErrors.lastProject = 'Превышен лимит символов в поле ввода';
     } else formErrors.lastProject = '';
 
@@ -73,7 +68,8 @@ export class Main extends Component {
       formErrors.stackTechnology ||
       formErrors.lastProject
     ) {
-      this.setState(() => ({
+      setState((prevState) => ({
+        ...prevState,
         nameError: formErrors.name,
         lastNameError: formErrors.lastName,
         dateOfBirthError: formErrors.dateOfBirth,
@@ -84,30 +80,26 @@ export class Main extends Component {
         lastProjectError: formErrors.lastProject,
       }));
     } else {
-      this.props.renameHeaderTitle(this.state.name, this.state.lastName);
-      this.setState({ formIsValid: true });
+      props.renameHeaderTitle(state.name, state.lastName);
+      setState((prevState) => ({ ...prevState, formIsValid: true }));
     }
   };
-
-  clearState = (event) => {
+  const clearState = (event) => {
     event.preventDefault();
-    this.props.renameHeaderTitle(this.state.formIsValid, this.state.name, this.state.lastName);
-    this.setState(formInitialState);
+    setState(formInitialState);
   };
 
-  render() {
-    return (
-      <>
-        <Form
-          getValuesFromForm={this.getValuesFromForm}
-          inputsValidate={this.inputsValidate}
-          clearState={this.clearState}
-          state={this.state}
-        />
-        <FormCompleted state={this.state} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Form
+        getValuesFromForm={getValuesFromForm}
+        inputsValidate={inputsValidate}
+        clearState={clearState}
+        state={state}
+      />
+      {/* <FormCompleted state={state} /> */}
+    </>
+  );
+};
 
 export default Main;
