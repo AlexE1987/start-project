a601c8ea06c6161b2a7ec8d58240b3047a9f0a88
AlexE1987 <evsikovad1987@gmail.com>
2021-10-19 17:09:12 +0400

    feat: add react-dom-router, implement basci components and navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import About from './components/pages/About/About';
+import Home from './components/pages/Home/Home';
+// import Product from './components/pages/Product';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/about" component={About} />
+        {/* <Route path="/product" component={Product} /> */}
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
