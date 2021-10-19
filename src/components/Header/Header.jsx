3eb464352bd0a3a99eec02e743ec1cb2835fb06a
AlexE1987 <evsikovad1987@gmail.com>
2021-10-19 17:25:58 +0400

    feat: basic components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Basket from '../Basket';
+import Navbar from '../Navbar';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+      <Basket />
+    </div>
+  );
+};
+
+export default Header;
