0447b8de1ed33c2f72291141541d245e8777a554
AlexE1987 <evsikovad1987@gmail.com>
2021-10-26 23:09:06 +0400

    implement fetch todoList and render data

---
+++ home-work-5/src/redux/actions/todoList.ts
@@ -0,0 +1,20 @@
+import { Dispatch } from "redux"
+import { TodoListActionTypes, ITodoListActions } from "../../types/todoList"
+
+export const fetchTodoList = () => {
+  return async (dispatch: Dispatch<ITodoListActions>) => {
+    try {
+      dispatch({type: TodoListActionTypes.GET_TODO_LIST});
+      const response = await fetch('http://localhost:3000/todo')
+      .then((response) => response.json())
+      .then((data) => {
+      console.log('333',data[0].description);
+      return data
+      })
+      dispatch({type: TodoListActionTypes.GET_TODO_LIST_SUCCESS, payload: response})
+    } catch (error) {
+      dispatch({type: TodoListActionTypes.GET_TODO_LIST_ERROR, payload: 'Error while downloading'});
+    }
+  }
+
+}
