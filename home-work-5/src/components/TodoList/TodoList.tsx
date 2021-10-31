03d2fb77a0b3d6087d0941840a46f1b69395645d
AlexE1987 <evsikovad1987@gmail.com>
2021-11-01 02:46:09 +0400

    init filters todolist

--- home-work-5/src/components/TodoList/TodoList.tsx
+++ home-work-5/src/components/TodoList/TodoList.tsx
@@ -1,5 +1,5 @@
 import './TodoList.css';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { IlistItem } from '../../types/todoListIt2';
@@ -8,32 +8,65 @@ import { postData } from '../../api/todoListApi';
 
 import TodoItem from './TodoItem/TodoItem';
 import TodoInput from './TodoInput/TodoInput';
+import TodoFilter from './TodoFilter/TodoFilter';
 
 const TodoList: FC = () => {
-const todoList = useTypedSelector((store) => store.updateTodoList.todoList);
-const dispatch = useDispatch();
+  const todoListInit = useTypedSelector((store) => store.updateTodoList.todoList);
+  const [todoList, setTodoList] = useState<any []>([]);
+  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
 
-useEffect(()=> {
-  dispatch(fetchTodoList())
-  },[dispatch])
+  useEffect(()=>  {
+    setTodoList(todoListInit);
+  },[todoListInit]);
 
-const onAddListItem = async (listItem: IlistItem) => {
-  const newId = Math.max(...todoList.map((todoItem) => todoItem.id)) + 1;
-  listItem.id = newId;
-  dispatch(addListItem(listItem));
-  postData(listItem)
-};
+
+  useEffect(()=> {
+    dispatch(fetchTodoList());
+  },[dispatch]);
+
+  const onAddListItem = async (listItem: IlistItem) => {
+    const newId = Math.max(...todoList.map((todoItem) => todoItem.id)) + 1;
+    listItem.id = newId;
+    dispatch(addListItem(listItem));
+    postData(listItem)
+  };
+
+  const filterCompleted = () => {
+    const forCompletedTodo = [...todoListInit];
+    const completedTodo = forCompletedTodo.filter((item) => item.isCompleted === true);
+    setTodoList(completedTodo);
+    setFilter('Completed');
+  };
+
+  const filterUncompleted = () => {
+    const forUncompletedTodo = [...todoListInit];
+    const uncompletedTodo = forUncompletedTodo.filter((item) => item.isCompleted === false);
+    setTodoList(uncompletedTodo);
+    setFilter('Uncompleted');
+  };
+
+  const filterFavorite = () => {
+    const forFavoriteTodo = [...todoListInit];
+    const favoriteTodo = forFavoriteTodo.filter((item) => item.isInFavorite === true);
+    setTodoList(favoriteTodo);
+    setFilter('Favorite');
+  };
 
   return (
     <div className="todo-list__wrapper">
       <div className="todo-list__container">
         <h3>My Todo List</h3>
         <TodoInput addListItem={onAddListItem}/>
-
+        <TodoFilter 
+          filterCompleted={filterCompleted}
+          filterUncompleted={filterUncompleted} 
+          filterFavorite={filterFavorite}/>
           <ul className="todo-list">
             {todoList.map((listItem) => 
             <TodoItem 
             key={listItem.id}
+            todoList={todoList}
             id={listItem.id}
             description={listItem.description}         
             isCompleted={listItem.isCompleted}         
